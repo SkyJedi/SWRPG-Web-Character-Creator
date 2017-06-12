c978eee84f7c36ef5dc499e6f505f9750c115d60
SkyJedi <skyjedi@gmail.com>
2017-06-12 13:43:03 -0500

    description page

--- src/pages/Description.js
+++ src/pages/Description.js
@@ -4,7 +4,50 @@ class Description extends Component {
 
   render() {
     return (
-      <h1>Description</h1>
+      <div className='box'>
+      <table style={{fontSize: '15px', float: 'left'}}>
+      <tbody>
+        <tr>
+          <td>Player Name:</td>
+          <td><input className='textinput' style={{width:'160px'}} ref="playerName" /></td>
+        </tr>
+        <tr>
+          <td>Character Name:</td>
+          <td><input className='textinput' style={{width:'160px'}} ref="characterName" /></td>
+        </tr>
+        <tr>
+          <td>Gender:</td>
+          <td><input className='textinput' style={{width:'160px'}} ref="geder" /></td>
+        </tr>
+        <tr>
+          <td>Age:</td>
+          <td><input className='textinput' style={{width:'160px'}} ref="Age" /></td>
+        </tr>
+        <tr>
+          <td>Height:</td>
+          <td><input className='textinput' style={{width:'160px'}} ref="height" /></td>
+        </tr>
+        <tr>
+          <td>Build:</td>
+          <td><input className='textinput' style={{width:'160px'}} ref="build" /></td>
+        </tr>
+        <tr>
+          <td>Hair:</td>
+          <td><input className='textinput' style={{width:'160px'}} ref="hair" /></td>
+        </tr>
+        <tr>
+          <td>Eyes:</td>
+          <td><input className='textinput' style={{width:'160px'}} ref="eyes" /></td>
+        </tr>
+      </tbody>
+      </table>
+      <div style={{marginLeft: '500px'}}>
+      <img src={`/Data/SpeciesImages/default.png`}/>
+      <button>Select Image</button>
+
+      </div>
+
+      </div>
     );
   }
 }
