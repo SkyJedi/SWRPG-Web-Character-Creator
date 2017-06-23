9b55fb4b9579338cfaffe40a1239c5ef14bd9528
SkyJedi <skyjedi@gmail.com>
2017-06-22 21:41:52 -0500

    Connectors are working in Specializations

--- src/pages/Specializations.js
+++ src/pages/Specializations.js
@@ -27,11 +27,26 @@ class Specializations extends Component {
     this.setState({selectedSpecialization: selectedSpecialization})
   }
 
+  topConnector(row, col) {
+    let background = '';
+    if (row === 'row1') background = 'transparent';
+    else if (this.state.selectedSpecialization.Talents[row][col].Direction.Up === true) background = 'black';
+    return background;
+  }
+
+  sideConnector(row, col) {
+    let background = '';
+    if (col === 'col4') background = 'transparent';
+    else if (this.state.selectedSpecialization.Talents[row][col].Direction.Right === true) background = 'black';
+
+    return background;
+  }
+
   render() {
     return (
 
       <div style={{float:'left', width: 'auto', height:'1000px'}}>
-        <div className='box' style={{height:'auto', width: 'auto'}}>
+        <div className='box' style={{height:'1000px', width: 'auto'}}>
           <b style={{margin: '5px', padding: '5px', float: 'left'}}>SPECIALIZATION:</b>
           <select style={{margin: '10px 5px 10px 0', padding: '5px', width: '180px', verticalAlign: 'top'}} ref='Specializations' onChange={this.selectSpecialization.bind(this)}>
             {Object.keys(this.state.Specializations).map((key)=>
@@ -42,13 +57,20 @@ class Specializations extends Component {
           <button style={{display: 'inline-block'}}>REMOVE</button>
           <div style={{fontSize: '20px', width: '98%', maxHeight: '240px', overflow: 'auto', margin: '5px'}} />
           {Object.keys(this.state.selectedSpecialization.Talents).map((row)=>
-            <div key={row} style={{width: 'auto', height: '130px', margin: '10px 5px'}}>
+            <div key={row} style={{width: '800px', height: 'auto', margin: '0px 0px 0px 20px'}}>
+              <div style={{verticalAlign: 'bottom', width:'30px', height: '30px', background: this.topConnector(row, 'col1'), marginLeft: '75px', display: 'inline-block'}}></div>
+              <div style={{verticalAlign: 'bottom', width:'30px', height: '30px', background: this.topConnector(row, 'col2'), marginLeft: '170px', display: 'inline-block'}}></div>
+              <div style={{verticalAlign: 'bottom', width:'30px', height: '30px', background: this.topConnector(row, 'col3'), marginLeft: '170px', display: 'inline-block'}}></div>
+              <div style={{verticalAlign: 'bottom', width:'30px', height: '30px', background: this.topConnector(row, 'col4'), marginLeft: '170px', display: 'inline-block'}}></div>
               {Object.keys(this.state.selectedSpecialization.Talents[row]).map((col)=>
-                <div key={col} className='box' style={{height:'100px', width: '150px'}}>
-                  <input type='checkbox' ref={row + col} checked={this.state.selectedSpecialization.Talents[row][col].isChecked} onChange={this.toggleCheckbox.bind(this, row, col)} />
-                  <label style={{fontSize:'15px'}}>{this.state.Talents[this.state.selectedSpecialization.Talents[row][col].Key].Name}</label>
-                  <div style={{fontSize: '10px', width: '98%', height: '60%', overflow: 'auto', margin: '5px'}} dangerouslySetInnerHTML={{__html: this.state.Talents[this.state.selectedSpecialization.Talents[row][col].Key].Description}} />
-                  <div style={{bottom: '0', float: 'right'}}><b>Cost: {this.state.selectedSpecialization.Talents[row][col].Cost}</b></div>
+                <div key={col}>
+                  <div className='box' style={{height:'100px', width: '150px', margin: '0px'}}>
+                    <input type='checkbox' ref={row + col} checked={this.state.selectedSpecialization.Talents[row][col].isChecked} onChange={this.toggleCheckbox.bind(this, row, col)} />
+                    <label style={{fontSize:'12px'}}>{this.state.Talents[this.state.selectedSpecialization.Talents[row][col].Key].Name}</label>
+                    <div style={{fontSize: '10px', width: '98%', height: '60%', overflow: 'auto', margin: '5px'}} dangerouslySetInnerHTML={{__html: this.state.Talents[this.state.selectedSpecialization.Talents[row][col].Key].Description}}></div>
+                    <div style={{bottom: '0', float: 'right'}}><b>Cost: {this.state.selectedSpecialization.Talents[row][col].Cost}</b></div>
+                  </div>
+                  <div style={{width:'20px', height: '30px', background: this.sideConnector(row, col), float: 'left', marginTop: '50px'}}></div>
                 </div>
               )}
             </div>
